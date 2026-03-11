cd264ba7d1748d5974723b0c847150262f834cdd
Mohammed <m.hamdoune8@gmail.com>
2026-03-11 14:48:57 +0100

    Lombok Logging

--- src/main/java/com/momed/spring7restmvc/controllers/BeerController.java
+++ src/main/java/com/momed/spring7restmvc/controllers/BeerController.java
@@ -1,11 +1,22 @@
 package com.momed.spring7restmvc.controllers;
 
+import com.momed.spring7restmvc.model.Beer;
 import com.momed.spring7restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
     public final BeerService beerService;
+
+    public Beer getBeerById(UUID id){
+
+        log.debug("getBeerById() called in controller. Id: {}", id);
+        return beerService.getBeerById(id);
+    }
 }
--- src/main/java/com/momed/spring7restmvc/service/BeerServiceImpl.java
+++ src/main/java/com/momed/spring7restmvc/service/BeerServiceImpl.java
@@ -2,14 +2,20 @@ package com.momed.spring7restmvc.service;
 
 import com.momed.spring7restmvc.model.Beer;
 import com.momed.spring7restmvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
+
+        log.debug("getBeerById() called in service. Id: {}", id);
         return Beer.builder()
                 .id(id)
                 .version(1)
